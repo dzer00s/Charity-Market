3abd5c8eef7848b1d0f18ff722690b1ed47a8942
dzer00s <amakadze@yandex.ru>
2021-03-16 17:00:24 +0300

    upd packages& added hooks

---
+++ src/hooks/useTypedSelector.ts
@@ -0,0 +1,5 @@
+import { RootState } from './../store/reducers/index';
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
+
+
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector
